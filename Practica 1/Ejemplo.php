71be3b62699d8eaaaecc7b41a0396368f7c12c16
Rciat05 <rciglesiaa05@gmail.com>
2024-08-15 23:19:07 -0600

    Cambios en el index Ejemplo

--- Practica 1/Ejemplo.php
+++ Practica 1/Ejemplo.php
@@ -1,3 +1,36 @@
 <?php
 
-echo "Hola";
+$nombre = "Roberto"
+$apellido = 'Iglesias'
+
+//Entero
+$Edad = 24;
+
+
+//Float
+$estatura = 1.70;
+
+
+//Booleano
+$vivo = true;
+
+
+var_dump($nombre)
+
+
+//Imprimir
+
+echo $nombre . "<br>";
+print_r($apellido)
+
+echo "<br><br>"
+
+echo $nombre,' ',$apellido,' ',$estatura;
+
+
+echo "<br><br>"
+var_dump($Edad)
+
+$Edad = 'Veinte y cuatro'
+
+echo "<br><br>"
